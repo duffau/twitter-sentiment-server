99f9c16eca6aa27f3a6e4656856960a49ddf1f06
christian <cd@palmtreestatistics.com>
2018-01-24 14:05:21 +0100

    Made a src folder with a mock Twitter channel

---
+++ src/listentwittermock.go
@@ -0,0 +1,63 @@
+package main
+
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+type Tweet struct {
+	Text string
+}
+
+func MagicEightBallTweet() Tweet {
+	answers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	}
+	return Tweet{answers[rand.Intn(20)]}
+}
+
+
+func sendMagicEightBallTweetsToChannel(limit int, ch chan Tweet) {
+	var randomDuration time.Duration
+	for i := 0; i < limit; i++ {
+		ch <- MagicEightBallTweet()
+		randomDuration = time.Duration(rand.Intn(1000)) * time.Millisecond 
+		//fmt.Printf("randomDuration = %v\n", randomDuration)
+		time.Sleep(randomDuration)
+	}
+	close(ch)
+}
+
+func mockTwitterChannel() <-chan Tweet {
+	ch := make(chan Tweet)
+	go sendMagicEightBallTweetsToChannel(20, ch)
+	return ch	
+}
+
+func listen(ch <-chan Tweet) {
+	for tweet := range ch {
+		fmt.Println(tweet)
+	} 
+
+}
--- src/main.go
+++ src/main.go
@@ -1,43 +1,81 @@
 package main
 
 import (
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/ekzhu/go-fasttext"
+	"time"
 	"net/http"
+	// "io/ioutil"
+	"github.com/gorilla/websocket"
 	"log"
-	"io/ioutil"
-	"strings"
-	//"github.com/dghubble/go-twitter/twitter"
-	//"github.com/dghubble/oauth1"
+	"bitbucket.com/chrduffau/wordvec"
 )
 
+var upgrader = websocket.Upgrader{}
 
-func myHandlerFunc(w http.ResponseWriter, req *http.Request) {
-	path := string(req.URL.Path[1:])
-	log.Println(path)
-	data, err := ioutil.ReadFile(path)
-	addContentType(w, path)
-	if err == nil {
-		w.Write(data)
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("404 - " + http.StatusText(404)))
-	}
+
+type Message struct {
+	Timestamp int64
+	Sentiment float64
+	Text string
 }
 
 
-func addContentType(w http.ResponseWriter, path string){
-	var contentType string
-
-	if strings.HasSuffix(path, ".html") {
-        contentType = "text/html"
-	} else if strings.HasSuffix(path, ".js") {
-		contentType = "application/javascript"
-	} else {
-		contentType = "text/plain"			
-	}
-	w.Header().Add("Content Type", contentType)
+type AppData struct {
+	ch <-chan Tweet
+	ft *fasttext.FastText
+	vgood []float64
+	vbad []float64
 }
+
+/*func indexHandler(w http.ResponseWriter, r *http.Request) {
+	html, _ := ioutil.ReadFile("./public/index.html")
+	w.Write(html)
+}
+*/
+
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}	
+}
+
+
+func (ad *AppData) websocketHandler(w http.ResponseWriter, r *http.Request) {
+	
+	var msg Message
+	var sentiment float64
+	var tweetVec  []float64
+	var tweet Tweet
+
+	conn, err := upgrader.Upgrade(w, r, nil)	
+	logError(err)
+
+	for {
+		tweet = <-ad.ch
+		log.Println(tweet)
+		tweetVec = wordvec.SentenceVec(ad.ft, tweet.Text)
+		sentiment = wordvec.Sentiment(ad.vgood, ad.vbad, tweetVec, wordvec.CosineSimilarity)
+		msg = Message{time.Now().Unix(), sentiment, tweet.Text}
+		err = conn.WriteJSON(msg)
+		if err != nil {
+			logError(err)
+			return
+		}
+	}
+}
+
 
 func main() {
-	http.HandleFunc("/", myHandlerFunc)
+
+	twitterChannel := mockTwitterChannel() 
+	ft := fasttext.NewFastText("./database/fasttext.db")
+	vecNormalizedGood, _ := wordvec.Normalize(wordvec.WordVec(ft, "good"))
+	vecNormalizedBad, _ := wordvec.Normalize(wordvec.WordVec(ft, "bad"))
+
+	ad := AppData{twitterChannel, ft, vecNormalizedGood, vecNormalizedBad}
+
+	http.Handle("/", http.FileServer(http.Dir("public/")))
+	http.HandleFunc("/websocket", ad.websocketHandler)
 	http.ListenAndServe(":8080", nil)
 }
